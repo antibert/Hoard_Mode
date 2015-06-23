020c9a26d9676c2b8b530df37554a04737b154ce
H3licopter <nyedlj@hotmail.co.uk>
2015-06-23 17:45:47 +0100

    stealing scripts from holdout and making stuff not work. woooo

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1,7 +1,9 @@
--- Generated from template
+-- hoard_mode template (I've build a basic script based off of holdout_example game mode. Most of this won't function properly at this time ~Nye)
 
-if CAddonTemplateGameMode == nil then
-	CAddonTemplateGameMode = class({})
+require( "hoard_mode_game_spawner" )
+
+if Choard_mode == nil then
+	Choard_mode = class({})
 end
 
 function Precache( context )
@@ -16,21 +18,116 @@ end
 
 -- Create the game mode when we activate
 function Activate()
-	GameRules.AddonTemplate = CAddonTemplateGameMode()
-	GameRules.AddonTemplate:InitGameMode()
+	GameRules.hoard_mode = Choard_modeGameMode()
+	GameRules.hoard_mode:InitGameMode()
+	self._entAncient = Entities:FindByName( nil, "dota_goodguys_fort" )
+	if not self._entAncient then
+		print( "Ancient entity not found!" )
+	end
+		
+	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_GOODGUYS, 5 )
+	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_BADGUYS, 0 )
+	
+	self:_ReadGameConfiguration()
+	GameRules:SetUseUniversalShopMode( true )
+	GameRules:SetHeroSelectionTime( 30.0 )
+	GameRules:SetPreGameTime( 5.0 )
+	GameRules:SetPostGameTime( 30.0 )
+	GameRules:SetTreeRegrowTime( 60.0 )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesOverride( true )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesVisible( false )
+
+--	GameRules:GetGameModeEntity():SetHUDVisible( DOTA_HUD_VISIBILITY_TOP_HEROES, false )
+
+	-- Hook into game events allowing reload of functions at run time (?????)
+	ListenToGameEvent( "npc_spawned", Dynamic_Wrap( Choard_modeGameMode, "OnNPCSpawned" ), self )
+	ListenToGameEvent( "player_reconnected", Dynamic_Wrap( Choard_modeGameMode, 'OnPlayerReconnected' ), self )
+	ListenToGameEvent( "entity_killed", Dynamic_Wrap( Choard_modeGameMode, 'OnEntityKilled' ), self )
+	ListenToGameEvent( "game_rules_state_change", Dynamic_Wrap( Choard_modeGameMode, "OnGameRulesStateChange" ), self )
+
+	-- Register OnThink with the game engine so it is called every 0.25 seconds (????)
+	GameRules:GetGameModeEntity():SetThink( "OnThink", self, 0.25 ) 
 end
 
-function CAddonTemplateGameMode:InitGameMode()
+-- Read and assign configurable keyvalues if applicable
+function Choard_modeGameMode:_ReadGameConfiguration()
+	local kv = LoadKeyValues( "scripts/maps/" .. GetMapName() .. ".txt" )
+	kv = kv or {} -- Handle the case where there is not keyvalues file
+	
+	end 
+
+	-- Verify spawners if random is set
+function Choard_modeGameMode:ChooseRandomSpawnInfo()
+	if #self._vRandomSpawnsList == 0 then
+		error( "Attempt to choose a random spawn, but no random spawns are specified in the data." )
+		return nil
+	end
+	return self._vRandomSpawnsList[ RandomInt( 1, #self._vRandomSpawnsList ) ]
+end
+
+
+-- Verify valid spawns are defined and build a table with them from the keyvalues file
+function Choard_modeGameMode:_ReadRandomSpawnsConfiguration( kvSpawns )
+	self._vRandomSpawnsList = {}
+	if type( kvSpawns ) ~= "table" then
+		return
+	end
+	for _,sp in pairs( kvSpawns ) do			-- Note "_" used as a shortcut to create a temporary throwaway variable
+		table.insert( self._vRandomSpawnsList, {
+			szSpawnerName = sp.SpawnerName or "",
+			szFirstWaypoint = sp.Waypoint or ""
+		} )
+	end
+end
+	
+	
+	
+	
+function Choard_modeGameMode:InitGameMode()
 	print( "Template addon is loaded." )
 	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
 end
 
 -- Evaluate the state of the game
-function CAddonTemplateGameMode:OnThink()
+function Choard_modeGameMode:OnThink()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		--print( "Template addon script is running." )
+		--print( "hoard_mode script is running." )
 	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
 		return nil
 	end
 	return 1
+end
+
+-- how the radiant wins le game lololololol xD top kek
+function Choard_modeGameMode:_CheckForDefeat()
+	if GameRules:State_Get() ~= DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		return
+	end
+
+	local bAllPlayersDead = true
+	for nPlayerID = 0, DOTA_MAX_TEAM_PLAYERS-1 do
+		if PlayerResource:GetTeam( nPlayerID ) == DOTA_TEAM_GOODGUYS then
+			if not PlayerResource:HasSelectedHero( nPlayerID ) then
+				bAllPlayersDead = false
+			else
+				local hero = PlayerResource:GetSelectedHeroEntity( nPlayerID )
+				if hero and hero:IsAlive() then
+					bAllPlayersDead = false
+				end
+			end
+		end
+	end
+
+	if bAllPlayersDead or not self._entAncient or self._entAncient:GetHealth() <= 0 then
+		GameRules:MakeTeamLose( DOTA_TEAM_GOODGUYS )
+		return
+	end
+end
+
+-- getting rid of un-needed frostivus particle stuff I guess? (might be useless code)
+
+function Choard_modeGameMode:_SpawnHeroClientEffects( hero, nPlayerID )
+	-- Spawn these effects on the client, since we don't need them to stay in sync or anything
+	-- ParticleManager:ReleaseParticleIndex( ParticleManager:CreateParticleForPlayer( "particles/generic_gameplay/winter_effects_hero.vpcf", PATTACH_ABSORIGIN_FOLLOW, hero, PlayerResource:GetPlayer( nPlayerID ) ) )	-- Attaches the breath effects to players for winter maps
+	ParticleManager:ReleaseParticleIndex( ParticleManager:CreateParticleForPlayer( "particles/frostivus_gameplay/frostivus_hero_light.vpcf", PATTACH_ABSORIGIN_FOLLOW, hero, PlayerResource:GetPlayer( nPlayerID ) ) )
 end

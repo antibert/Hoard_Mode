79c820178f9d6483c71b6e2e9d9c64f23c9cd57c
robert <robert.horvers@gmail.com>
2015-09-10 19:19:06 +0200

    candidate numbre 3

--- scripts/vscripts/spawners.lua
+++ scripts/vscripts/spawners.lua
@@ -1319,7 +1319,7 @@ function Spawners:SpawnFriend()
 			break
 		end
 		if bottomFriends[j] ~= nil then
-			if bottomFriends[j]:GetHealth() == 0 then
+			if bottomFriends[j]:IsNull() == true then
 				bottomFriends[j] = nil
 			end
 		end

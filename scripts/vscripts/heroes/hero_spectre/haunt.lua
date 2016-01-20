33bec28ca65f1195d287024bdce2df3fb607521e
Dylan Kennet <diellan@gmail.com>
2016-01-20 23:01:21 +0200

    haunt fix

--- scripts/vscripts/heroes/hero_spectre/haunt.lua
+++ scripts/vscripts/heroes/hero_spectre/haunt.lua
@@ -14,7 +14,6 @@ function Haunt( event )
     local caster = event.caster
     local player = caster:GetPlayerID()
     local ability = event.ability
-    local target = event.target
     local unit_name = caster:GetUnitName()
     local duration = ability:GetLevelSpecialValueFor( "duration", ability:GetLevel() - 1 )
     local outgoingDamage = ability:GetLevelSpecialValueFor( "illusion_damage_outgoing", ability:GetLevel() - 1 )
@@ -25,50 +24,53 @@ function Haunt( event )
     -- Stop any actions of the caster otherwise its obvious which unit is real
     caster:Stop()
 
-    -- Spawn illusions
-    local origin = target:GetAbsOrigin()
+    local heroes = event.target_entities
+    for _,target in pairs(heroes) do
+        -- Spawn illusions
+        local origin = target:GetAbsOrigin()
 
-    -- handle_UnitOwner needs to be nil, else it will crash the game.
-    local illusion = CreateUnitByName(unit_name, origin, true, caster, nil, caster:GetTeamNumber())
-    illusion:SetPlayerID(caster:GetPlayerID())
-    illusion:SetControllableByPlayer(player, true)
+        -- handle_UnitOwner needs to be nil, else it will crash the game.
+        local illusion = CreateUnitByName(unit_name, origin, true, caster, nil, caster:GetTeamNumber())
+        illusion:SetPlayerID(caster:GetPlayerID())
+        illusion:SetControllableByPlayer(player, true)
 
-    illusion:SetAngles( casterAngles.x, casterAngles.y, casterAngles.z )
+        illusion:SetAngles( casterAngles.x, casterAngles.y, casterAngles.z )
 
-    -- Level Up the unit to the casters level
-    local casterLevel = caster:GetLevel()
-    for i=1,casterLevel-1 do
-        illusion:HeroLevelUp(false)
-    end
-
-    -- Set the skill points to 0 and learn the skills of the caster
-    illusion:SetAbilityPoints(0)
-    for abilitySlot=0,15 do
-        local ability = caster:GetAbilityByIndex(abilitySlot)
-        if ability ~= nil then
-            local abilityLevel = ability:GetLevel()
-            local abilityName = ability:GetAbilityName()
-            local illusionAbility = illusion:FindAbilityByName(abilityName)
-            illusionAbility:SetLevel(abilityLevel)
+        -- Level Up the unit to the casters level
+        local casterLevel = caster:GetLevel()
+        for i=1,casterLevel-1 do
+            illusion:HeroLevelUp(false)
         end
-    end
 
-    -- Recreate the items of the caster
-    for itemSlot=0,5 do
-        local item = caster:GetItemInSlot(itemSlot)
-        if item ~= nil then
-            local itemName = item:GetName()
-            local newItem = CreateItem(itemName, illusion, illusion)
-            illusion:AddItem(newItem)
+        -- Set the skill points to 0 and learn the skills of the caster
+        illusion:SetAbilityPoints(0)
+        for abilitySlot=0,15 do
+            local ability = caster:GetAbilityByIndex(abilitySlot)
+            if ability ~= nil then
+                local abilityLevel = ability:GetLevel()
+                local abilityName = ability:GetAbilityName()
+                local illusionAbility = illusion:FindAbilityByName(abilityName)
+                illusionAbility:SetLevel(abilityLevel)
+            end
         end
+
+        -- Recreate the items of the caster
+        for itemSlot=0,5 do
+            local item = caster:GetItemInSlot(itemSlot)
+            if item ~= nil then
+                local itemName = item:GetName()
+                local newItem = CreateItem(itemName, illusion, illusion)
+                illusion:AddItem(newItem)
+            end
+        end
+
+        -- Set the unit as an illusion
+        -- modifier_illusion controls many illusion properties like +Green damage not adding to the unit damage, not being able to cast spells and the team-only blue particle
+        illusion:AddNewModifier(caster, ability, "modifier_illusion", { duration = duration, outgoing_damage = outgoingDamage, incoming_damage = incomingDamage })
+
+        -- Without MakeIllusion the unit counts as a hero, e.g. if it dies to neutrals it says killed by neutrals, it respawns, etc.
+        illusion:MakeIllusion()
+        -- Set the illusion hp to be the same as the caster
+        illusion:SetHealth(caster:GetHealth())
     end
-
-    -- Set the unit as an illusion
-    -- modifier_illusion controls many illusion properties like +Green damage not adding to the unit damage, not being able to cast spells and the team-only blue particle
-    illusion:AddNewModifier(caster, ability, "modifier_illusion", { duration = duration, outgoing_damage = outgoingDamage, incoming_damage = incomingDamage })
-
-    -- Without MakeIllusion the unit counts as a hero, e.g. if it dies to neutrals it says killed by neutrals, it respawns, etc.
-    illusion:MakeIllusion()
-    -- Set the illusion hp to be the same as the caster
-    illusion:SetHealth(caster:GetHealth())
 end

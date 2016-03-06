6594274a79ef2ebd05a25351eec31a77351092dd
Dylan Kennet <diellan@gmail.com>
2016-03-06 08:04:03 +0200

    outworld devourer

---
+++ scripts/vscripts/heroes/hero_obsidian_destroyer/sanity_eclipse.lua
@@ -0,0 +1,47 @@
+--[[Sanity Eclipse
+	Author: chrislotix
+	Date: 08.01.2015.
+	]]
+
+
+function SanityEclipseDamage( keys )
+	
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+	local od_int = caster:GetIntellect()
+	local mana = target:GetMaxMana()
+	local dmg_multiplier = ability:GetLevelSpecialValueFor("damage_multiplier", (ability:GetLevel() -1))
+	local mana_drain = ability:GetLevelSpecialValueFor("mana_drain", (ability:GetLevel() -1))
+	local has_scepter = caster:HasScepter()
+
+	if has_scepter then
+		dmg_multiplier = ability:GetLevelSpecialValueFor("damage_multiplier_scepter", (ability:GetLevel() -1))
+	end
+
+	local damage_table = {}
+
+	damage_table.attacker = caster
+	damage_table.damage_type = ability:GetAbilityDamageType()
+	damage_table.ability = ability
+	damage_table.victim = target
+
+	target:SetMana(mana* mana_drain/100)
+
+	damage_table.damage = od_int * dmg_multiplier
+	ApplyDamage(damage_table)
+end
+
+
+	
+
+
+
+
+
+
+
+	
+
+
+	

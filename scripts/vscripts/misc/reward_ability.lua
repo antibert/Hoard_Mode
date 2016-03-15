b7c90620de67a5f169185f811c4e1c3044fe84eb
Dylan Kennet <diellan@gmail.com>
2016-03-15 21:10:49 +0200

    fixes

--- scripts/vscripts/misc/reward_ability.lua
+++ scripts/vscripts/misc/reward_ability.lua
@@ -3,11 +3,12 @@ function GrantBuff(keys)
 	local building = Entities:FindByName(nil, keys.unit_to_target)
 	if building ~= nil then
 		local ability = building:FindAbilityByName(keys.ability_to_grant)
-		ShowMessage("The strength of your Ancient has increased!")
 		if ability == nil then
+			DebugPrint("Adding " .. keys.ability_to_grant .. " not found, adding")
 			building:AddAbility(keys.ability_to_grant)
 			building.SetLevel(1)
 		else
+			DebugPrint("Adding " .. keys.ability_to_grant .. " found at level " .. ability:GetLevel())
 			ability:SetLevel(ability:GetLevel() + 1)
 		end
 	else

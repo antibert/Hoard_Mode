698ec71652bfba43606cc5ef42dcd20ff4cf4406
Dylan Kennet <diellan@gmail.com>
2016-07-23 20:46:21 +0300

    1.19b

--- scripts/vscripts/heroes/hero_pugna/nether_ward.lua
+++ scripts/vscripts/heroes/hero_pugna/nether_ward.lua
@@ -82,3 +82,78 @@ function NetherWardMana( event )
 		hero.OldMana = hero:GetMana()
 	end
 end
+
+--[[Kill wolves on resummon
+	Author: Noya
+	Date: 20.01.2015.]]
+
+function KillWard( event )
+	local caster = event.caster
+	local targets = caster.wards or {}
+	local UnitLimit = event.UnitLimit
+	local NewUnits = {}
+	for _,unit in pairs(targets) do
+		if unit and IsValidEntity(unit) and unit:IsAlive() then
+			UnitLimit = UnitLimit - 1
+			if UnitLimit < 1 then
+				unit:ForceKill(true)
+				table.remove(caster.wards, _)
+			else
+				table.insert(NewUnits, unit)
+			end
+		end
+	end
+
+	caster.wards = NewUnits
+end
+
+-- Set the units looking at the same point of the caster
+function SetUnitsMoveForward( event )
+	local caster = event.caster
+	local target = event.target
+
+	-- Add the target to a table on the caster handle, to find them later
+	table.insert(caster.wards, target)
+end
+
+function SelfDetonate( keys )
+	local caster = keys.caster
+
+	caster:ForceKill(true)
+end
+
+function WardExplode(keys)
+	local caster = keys.caster
+	local sound_explode = keys.sound_explode
+	local particle_explode = keys.particle_explode
+	local owner = caster:GetOwnerEntity()
+
+	if owner:HasScepter() then
+		local pugna = Entities:FindByName(nil, "npc_dota_hero_pugna")
+		local ability = pugna:FindAbilityByName("pugna_nether_blast")
+		local ability_level = ability:GetLevel() - 1
+
+		if ability_level >= 0 then
+			-- Parameters
+			local damage = ability:GetAbilityDamage()
+			local radius = ability:GetLevelSpecialValueFor("radius", ability_level)
+			local caster_loc = caster:GetAbsOrigin()
+
+			-- Play sound
+			caster:EmitSound(sound_explode)
+
+			-- Fire particle
+			local explosion_pfx = ParticleManager:CreateParticle(particle_explode, PATTACH_ABSORIGIN, caster)
+			ParticleManager:SetParticleControl(explosion_pfx, 0, caster_loc)
+			ParticleManager:SetParticleControl(explosion_pfx, 1, Vector(radius,radius,radius))
+
+			-- Find nearby enemies
+			local near_units = FindUnitsInRadius(caster:GetTeamNumber(), caster_loc, nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_BUILDING, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_CLOSEST, false)
+
+			-- Damage enemies
+			for _,enemy in pairs(near_units) do
+				ApplyDamage({attacker = caster, victim = enemy, ability = ability, damage = damage, damage_type = DAMAGE_TYPE_MAGICAL})
+			end
+		end
+	end
+end

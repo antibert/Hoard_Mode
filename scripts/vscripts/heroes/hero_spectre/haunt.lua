e5e2485be089ed7541bcc4b40b8cbd3af55724eb
Dylan Kennet <diellan@gmail.com>
2016-11-04 16:43:56 +0200

    rubick haunt closes #51

--- scripts/vscripts/heroes/hero_spectre/haunt.lua
+++ scripts/vscripts/heroes/hero_spectre/haunt.lua
@@ -38,7 +38,9 @@ function CreateIllusion(caster, ability, target)
             local abilityLevel = ability:GetLevel()
             local abilityName = ability:GetAbilityName()
             local illusionAbility = illusion:FindAbilityByName(abilityName)
-            illusionAbility:SetLevel(abilityLevel)
+            if illusionAbility ~= nil then
+                illusionAbility:SetLevel(abilityLevel)
+            end
         end
     end
 

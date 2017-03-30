f050a588a75c494ca39cd6615d318e425a3c11e9
DankBudd <evilsword54@yahoo.com>
2017-03-29 23:52:53 -0400

    Update voodoo.lua

--- scripts/vscripts/heroes/hero_shadow_shaman/voodoo.lua
+++ scripts/vscripts/heroes/hero_shadow_shaman/voodoo.lua
@@ -9,15 +9,21 @@ function voodoo_start( keys )
 	local ability = keys.ability
 	local ability_level = ability:GetLevel() - 1
 	local target = keys.target
-
 	local duration = ability:GetLevelSpecialValueFor("duration", ability_level)
-	
+	local radius = ability:GetSpecialValueFor("radius")
+
 	if target:IsIllusion() then
 		target:ForceKill(true)
+		EmitSoundOn("Hero_ShadowShaman.Hex.Target", target)
 	else
 		if target:GetTeam() ~= caster:GetTeam() and target:TriggerSpellAbsorb(ability) then
 			return
 		end
-		target:AddNewModifier(caster, ability, "modifier_voodoo_lua", {duration = duration})
+		EmitSoundOn("Hero_ShadowShaman.Hex.Target", target)
+		local units = FindUnitsInRadius(caster:GetTeam(), target:GetAbsOrigin(), nil, radius, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), ability:GetAbilityTargetFlags(), FIND_ANY_ORDER, false)
+		for _,unit in pairs(units) do
+			unit:AddNewModifier(caster, ability, "modifier_shadow_shaman_voodoo", {duration = duration})
+			local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_shadowshaman/shadowshaman_voodoo.vpcf", PATTACH_ABSORIGIN_FOLLOW, unit)
+		end	
 	end
 end

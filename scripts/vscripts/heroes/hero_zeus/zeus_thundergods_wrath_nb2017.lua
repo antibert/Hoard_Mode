e43e0135564a057db013b8616d7489fe28e2e291
DankBudd <evilsword54@yahoo.com>
2017-04-27 03:32:30 -0400

    Update zeus_thundergods_wrath_nb2017.lua

--- scripts/vscripts/heroes/hero_zeus/zeus_thundergods_wrath_nb2017.lua
+++ scripts/vscripts/heroes/hero_zeus/zeus_thundergods_wrath_nb2017.lua
@@ -30,20 +30,22 @@ function zeus_thundergods_wrath_nb2017:OnSpellStart()
 		local heroes = HeroList:GetAllHeroes()
 		for i=1,#heroes do
 			local hero = heroes[i]
-			if hero ~= nil and hero:GetTeamNumber() == self:GetCaster():GetTeamNumber() then
-				local vStartPosition = hero:GetOrigin() + Vector( 0, 0, 4000.0 )
-				local nFXIndex = ParticleManager:CreateParticle( "particles/units/heroes/hero_zuus/zuus_thundergods_wrath.vpcf", PATTACH_CUSTOMORIGIN, self:GetCaster() )
-				ParticleManager:SetParticleControl( nFXIndex, 0, vStartPosition )
-				ParticleManager:SetParticleControlEnt( nFXIndex, 1, hero, PATTACH_POINT_FOLLOW, "attach_hitloc", hero:GetOrigin(), true )
-				ParticleManager:ReleaseParticleIndex( nFXIndex )
-				EmitSoundOn( "Hero_Zuus.GodsWrath.Target" , hero )
+			if hero then
+				if hero:IsRealHero() and hero:GetTeamNumber() == self:GetCaster():GetTeamNumber() then
+					local vStartPosition = hero:GetOrigin() + Vector( 0, 0, 4000.0 )
+					local nFXIndex = ParticleManager:CreateParticle( "particles/units/heroes/hero_zuus/zuus_thundergods_wrath.vpcf", PATTACH_CUSTOMORIGIN, self:GetCaster() )
+					ParticleManager:SetParticleControl( nFXIndex, 0, vStartPosition )
+					ParticleManager:SetParticleControlEnt( nFXIndex, 1, hero, PATTACH_POINT_FOLLOW, "attach_hitloc", hero:GetOrigin(), true )
+					ParticleManager:ReleaseParticleIndex( nFXIndex )
+					EmitSoundOn( "Hero_Zuus.GodsWrath.Target" , hero )
 
-				local hCloud = CreateUnitByName( "npc_dota_zeus_cloud", hero:GetOrigin(), true, self:GetCaster(), self:GetCaster(), self:GetCaster():GetTeamNumber() )
-				hCloud:SetOwner( self:GetCaster() )
-				hCloud:SetControllableByPlayer( self:GetCaster():GetPlayerOwnerID(), false )
-				hCloud:AddNewModifier( self:GetCaster(), self, "modifier_zuus_cloud", {} )
-				hCloud:AddNewModifier( self:GetCaster(), self, "modifier_kill", { duration = self:GetSpecialValueFor( "cloud_duration" ) } )
-				FindClearSpaceForUnit( hCloud, hero:GetOrigin(), true )
+					local hCloud = CreateUnitByName( "npc_dota_zeus_cloud", hero:GetOrigin(), true, self:GetCaster(), self:GetCaster(), self:GetCaster():GetTeamNumber() )
+					hCloud:SetOwner( self:GetCaster() )
+					hCloud:SetControllableByPlayer( self:GetCaster():GetPlayerOwnerID(), false )
+					hCloud:AddNewModifier( self:GetCaster(), self, "modifier_zuus_cloud", {} )
+					hCloud:AddNewModifier( self:GetCaster(), self, "modifier_kill", { duration = self:GetSpecialValueFor( "cloud_duration" ) } )
+					FindClearSpaceForUnit( hCloud, hero:GetOrigin(), true )
+				end
 			end
 		end
 	end

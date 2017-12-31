6a1fa38a2550fb8d9baf244a20635903a68fdf5e
SirPannbiff <34649219+SirPannbiff@users.noreply.github.com>
2017-12-31 21:11:07 +0100

    Various fixes

--- scripts/vscripts/heroes/hero_visage/soul_assumption.lua
+++ scripts/vscripts/heroes/hero_visage/soul_assumption.lua
@@ -12,6 +12,6 @@ function SoulAssumption( keys )
 	local units = FindUnitsInRadius(caster:GetTeam(), target:GetAbsOrigin(), nil, radius, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), ability:GetAbilityTargetFlags(), FIND_ANY_ORDER, false)
 	for _,unit in pairs(units) do
 		ApplyDamage({victim = unit, attacker = caster, ability = ability, damage = damage, damage_type = ability:GetAbilityDamageType()})
-		local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_visage/visage_soul_assumption_beam.vpcf", follow_origin, caster)
+		--local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_visage/visage_soul_assumption_beam.vpcf", follow_origin, caster)
 	end
 end

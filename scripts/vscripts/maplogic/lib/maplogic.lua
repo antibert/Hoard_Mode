44756d21392b257b74c2d7271483783b9e824868
Dylan Kennet <diellan@gmail.com>
2018-06-10 20:10:27 +0300

    1.26b worth working maps

--- scripts/vscripts/maplogic/lib/maplogic.lua
+++ scripts/vscripts/maplogic/lib/maplogic.lua
@@ -38,11 +38,18 @@ function mapLogic:Init(keys)
 	DebugPrint(printPrefix .. messageStarting)
 
 	self.MAP = GetMapName()
-	self:SetDifficultyValues(keys)
-	self:SetFriendlyBuildings()
-	self:SetEnemyBuildings()
 
-	self:SetSpawns()
+	ShowMessage('Map name when called upon in the test client:' .. self.MAP)
+	local mapData = mapInfo[string.lower(self.MAP)]
+	if mapData == nil then
+		mapData = mapInfo.Default
+	end
+
+	self:SetDifficultyValues(keys)
+	self:SetFriendlyBuildings(mapData)
+	self:SetEnemyBuildings(mapData)
+
+	self:SetSpawns(mapData)
 end
 
 -- Function that will set various settings based on difficulty
@@ -91,7 +98,7 @@ function mapLogic:SetDifficultyValues(keys)
 end
 
 -- Function that will grant the abilities to the friendly towers
-function mapLogic:SetFriendlyBuildings()
+function mapLogic:SetFriendlyBuildings(mapInfo)
 	-- Find all friendly buildings on the map
 	local buildings = FindUnitsInRadius(DOTA_TEAM_GOODGUYS, Vector(0,0,0), nil, 20000, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_BUILDING, DOTA_UNIT_TARGET_FLAG_INVULNERABLE, FIND_ANY_ORDER, false)
 
@@ -103,7 +110,7 @@ function mapLogic:SetFriendlyBuildings()
 		DebugPrint(building_name)
 
 		-- Identify the building type
-		if string.find(building_name, "tower") then
+		if building:IsTower() then
 			building:AddAbility("tower_aura")
 			local tower_overpower = building:FindAbilityByName("tower_aura")
 			tower_overpower:SetLevel(0)
@@ -119,6 +126,18 @@ function mapLogic:SetFriendlyBuildings()
 			building:AddAbility("reward_global_offense_penalty")
 			local ancient_ability = building:FindAbilityByName("reward_global_offense_penalty")
 			ancient_ability:SetLevel(1)
+
+			if (mapInfo.topLaneSpawner == nil and string.find(building_name, "top_1")) then
+				tower_overpower:SetLevel(4)
+				tower_stats:SetLevel(10)
+				tower_splash:SetLevel(4)
+			end
+
+			if (mapInfo.botLaneSpawner == nil and string.find(building_name, "bot_1")) then
+				tower_overpower:SetLevel(4)
+				tower_stats:SetLevel(10)
+				tower_splash:SetLevel(4)
+			end
 
 		elseif string.find(building_name, "rax_melee") then
 			building:AddAbility("reward_global_armor_penalty")
@@ -136,7 +155,7 @@ function mapLogic:SetFriendlyBuildings()
 			local building_stats = building:FindAbilityByName("building_stats")
 			building_stats:SetLevel(0)
 
-		elseif string.find(building_name, "fort") then
+		elseif building:IsFort() then
 			DebugPrint("Adding abilities to ancient")
 			-- Add passive buff
 			building:AddAbility("global_armor_buff")
@@ -162,9 +181,10 @@ function mapLogic:SetFriendlyBuildings()
 end
 
 -- Function that will grant the abilities to the enemy towers
-function mapLogic:SetEnemyBuildings()
+function mapLogic:SetEnemyBuildings(mapInfo)
 	-- Find all enemy buildings on the map
 	local buildings = FindUnitsInRadius(DOTA_TEAM_GOODGUYS, Vector(0,0,0), nil, 20000, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_BUILDING, DOTA_UNIT_TARGET_FLAG_INVULNERABLE, FIND_ANY_ORDER, false)
+
 
 	-- Iterate through each one
 	for _, building in pairs(buildings) do
@@ -174,12 +194,16 @@ function mapLogic:SetEnemyBuildings()
 		DebugPrint(building_name)
 
 		-- Identify the building type
-		if string.find(building_name, "tower") then
+		if building:IsTower() then
 			DebugPrint("Adding abilities to enemy tower")
 
 			-- Add abilities
 			building:AddAbility("reward_global_offense_buff")
 			local ancient_ability = building:FindAbilityByName("reward_global_offense_buff")
+			ancient_ability:SetLevel(1)
+
+			building:AddAbility("tower_stats")
+			local tower_stats = building:FindAbilityByName("tower_stats")
 
 			building:AddAbility("tower_aura")
 			local tower_aura = building:FindAbilityByName("tower_aura")
@@ -187,20 +211,20 @@ function mapLogic:SetEnemyBuildings()
 			building:AddAbility("tower_splash")
 			local tower_splash = building:FindAbilityByName("tower_splash")
 
-			if string.find(building_name, "tower1") then
-				ancient_ability:SetLevel(1)
+			if string.find(building_name, "1") then
+				tower_stats:SetLevel(1)
 				tower_aura:SetLevel(1)
 				tower_splash:SetLevel(1)
-			elseif string.find(building_name, "tower2") then
-				ancient_ability:SetLevel(1)
+			elseif string.find(building_name, "2") then
+				tower_stats:SetLevel(4)
 				tower_aura:SetLevel(2)
 				tower_splash:SetLevel(2)
-			elseif string.find(building_name, "tower3") then
-				ancient_ability:SetLevel(1)
+			elseif string.find(building_name, "3") then
+				tower_stats:SetLevel(7)
 				tower_aura:SetLevel(3)
 				tower_splash:SetLevel(3)
-			elseif string.find(building_name, "tower4") then
-				ancient_ability:SetLevel(1)
+			elseif string.find(building_name, "4") then
+				tower_stats:SetLevel(10)
 				tower_aura:SetLevel(4)
 				tower_splash:SetLevel(4)
 			end
@@ -219,7 +243,9 @@ function mapLogic:SetEnemyBuildings()
 			local reward_global_armor_buff = building:FindAbilityByName("reward_global_armor_buff")
 			reward_global_armor_buff:SetLevel(1)
 
-		elseif string.find(building_name, "fort") then
+		elseif building:IsFort() then
+			-- building:SetInvulnCount(10)
+
 			-- Add passive buff // Ability levels don't work properly
 			building:AddAbility("global_armor_buff")
 			local global_armor_buff = building:FindAbilityByName("global_armor_buff")
@@ -242,11 +268,6 @@ end
 require('Spawners')
 
 -- function that sets the timers for all the wave spawns
-function mapLogic:SetSpawns()
-	ShowMessage('Map name when called upon in the test client:' .. GetMapName())
-	local mapData = mapInfo[string.lower(GetMapName())]
-	if mapData == nil then
-		mapData = mapInfo.Default
-	end
-	Spawners:StartSpawners(self.DIFFICULTY, self.PLAYERS, mapData)
+function mapLogic:SetSpawns(mapInfo)
+	Spawners:StartSpawners(self.DIFFICULTY, self.PLAYERS, mapInfo)
 end
